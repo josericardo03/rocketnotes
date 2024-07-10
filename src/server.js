a2214fd2147aba0ae37c46274afa20d3551db77b
josericardo03 <borges2001jose@gmail.com>
2024-07-10 12:54:18 -0400

    teste

---
+++ src/server.js
@@ -0,0 +1,39 @@
+require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations");
+const AppError = require("./utils/AppError");
+// importando express
+const express = require("express");
+const routes = require("./routes");
+const uploadConfig = require("./configs/upload");
+
+migrationsRun();
+//inicializar ele
+
+const app = express();
+// transformando em json os arquivos
+
+app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
+app.use(routes);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
+//Endereço da porta
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
